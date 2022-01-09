91b11116537992eef51fd72b9b898f5f0015e903
missingfragment <hdelossantos94@gmail.com>
2022-01-09 16:34:59 -0600

    Image View now scales and centers properly.

--- user_interface/image_view_window.py
+++ user_interface/image_view_window.py
@@ -15,17 +15,33 @@ class ImageViewWindow(DefaultWindow):
 
         self.mainframe = ttk.Frame(self, padding=10)
         self.mainframe.grid(column=0, row=0, sticky=NSEW)
+        self.mainframe.bind("<Configure>", self.on_resize)
 
-        label = ttk.Label(self.mainframe, text=f"{path}")
-        label.grid(column=0, row=1, sticky=NSEW)
+        self.image_size = (0, 0)
 
         try:
             self.image = Image.open(path)
         except OSError:
             print("Failed to open image.")
+
         self.photoimage = ImageTk.PhotoImage(self.image)
 
         self.image_label = ttk.Label(
             self.mainframe, image=self.photoimage)
 
-        self.image_label.grid(column=0, row=0, sticky=NSEW)
+        self.image_label.place(relx=.5, rely=.5, anchor=CENTER)
+
+    def on_resize(self, event):
+        new_size = (event.width, event.height)
+        if new_size == self.image_size or new_size[0] < 20 or new_size[1] < 20:
+            return
+        new_image = self.image.copy()
+        new_image.thumbnail(new_size)
+        self.photoimage = ImageTk.PhotoImage(new_image)
+
+        self.image_size = new_size
+
+        self.update_image()
+
+    def update_image(self):
+        self.image_label['image'] = self.photoimage
