f825be10aaab6514605468f318ba34f862b30add
missingfragment <hdelossantos94@gmail.com>
2022-01-09 17:35:12 -0600

    Folder View now re-grids dynamically.

--- user_interface/folder_view_window.py
+++ user_interface/folder_view_window.py
@@ -22,10 +22,23 @@ class FolderViewWindow(DefaultWindow):
 
         self.scrollbar.grid(column=1, row=0, sticky=NSEW)
 
+        self.thumb_size = (200, 200)
+
         self.images = []
         self.icons = []
 
+        self.view_size = (0, 0)
+
         self.update_folder(path)
+
+        self.bind("<Configure>", self.on_resize)
+
+    def on_resize(self, event):
+        new_size = (event.width, event.height)
+        if new_size == self.view_size or new_size[0] < self.thumb_size[0] or new_size[1] < self.thumb_size[1]:
+            return
+
+        self.update_layout()
 
     def setup_scrollbar(self) -> Scrollbar:
         scrollbar = ttk.Scrollbar(
@@ -36,7 +49,7 @@ class FolderViewWindow(DefaultWindow):
         return scrollbar
 
     def update_folder(self, folder: Path) -> None:
-        thumb_size = (200, 200)
+        thumb_size = self.thumb_size
 
         self.images: Image = []
         self.icons = []
@@ -55,14 +68,6 @@ class FolderViewWindow(DefaultWindow):
             thumbnail.thumbnail(size=thumb_size)
             self.images.append(thumbnail)
 
-        max_width = self.winfo_width()
-        col_count = max_width // thumb_size[0]
-
-        if col_count <= 0:
-            col_count = 1
-
-        row: int = 0
-        col: int = 0
         i: int = 0
         for image in self.images:
             photoimage = ImageTk.PhotoImage(image=image)
@@ -73,17 +78,25 @@ class FolderViewWindow(DefaultWindow):
             ))
 
             label = self.icons[i]
+            i += 1
 
             label.image = photoimage
 
+    def update_layout(self) -> None:
+        max_width = self.winfo_width()
+        col_count = max_width // self.thumb_size[0]
+
+        if col_count <= 0:
+            col_count = 1
+
+        row: int = 0
+        col: int = 0
+        for label in self.icons:
+
             label.grid(column=col, row=row, sticky=NSEW)
-
-            print(f"{col}, {row}")
-
-            i += 1
 
             col += 1
 
-            if col > col_count:
+            if col >= col_count:
                 col = 0
                 row += 1
